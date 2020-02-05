ccb3288885facde2f06c862cd0a715867b6f62c4
MattiasVerhoeven <neveorcloud@gmail.com>
2020-02-05 20:39:18 +0100

    change getDayByDate function so it also uses 0-11 for monthNumber instead of 1-12

--- src/utils/date.helpers.js
+++ src/utils/date.helpers.js
@@ -48,7 +48,7 @@ export const getDayByDayNumber = (dayNumber) => {
 }
 export const getDayByDate = (month, dayOfMonth, year) =>{
     let dateString;
-    if(typeof month === 'number') if(month<0||month>11) throw Error("variable should be between 0-11."); else dateString = months[month-1].name + ' ' + dayOfMonth +',' + year;
+    if(typeof month === 'number') if(month<0||month>11) throw Error("variable should be between 0-11."); else dateString = months[month].name + ' ' + dayOfMonth +',' + year;
     else if(typeof month === 'string') dateString = month + ' ' + dayOfMonth +',' + year;
     else throw Error(`variable : "${month}" is not a number or a string.` );
 
