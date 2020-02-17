44cd89e6c561a35f87429d511b3e50fd63699a80
MattiasVerhoeven <neveorcloud@gmail.com>
2020-02-17 21:32:29 +0100

    add LocalStorage and SessionStorage types

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-import { randomHexColor} from 'halfeind-utils'
+import { randomHexColor, LocalStorage} from 'halfeind-utils'
 export default class App extends Component {
 
   render () {
-    
+    LocalStorage.set('a','b');
     return (
        <> 
         <div style={{width:'100%',height:'100%',backgroundColor:randomHexColor()}}></div>
